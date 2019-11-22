0afdbef62eda2f4cba29a816eabb648383aa3fcc
itsuka tomoya <itsuka@irvs.ait.kyushu-u.ac.jp>
2019-11-22 19:13:53 +0900

    subtask_nodes update

--- tms_ts/tms_ts_test_project/tms_ts_test/subtask_nodes.py
+++ tms_ts/tms_ts_test_project/tms_ts_test/subtask_nodes.py
@@ -10,6 +10,7 @@ import random
 import json
 
 from tms_msg_ur.srv import SpeakerSrv
+from geometry_msgs.msg import PoseStamped
 
 def main(args=None):
     global executor
@@ -103,8 +104,31 @@ class SubtaskMove(SubtaskNodeBase):
         return 9001
     
     async def service_callback(self, request, response):
+        place_id = request["PLACE_ID"]
+        position = request["position"]
+        orientation = request["orientation"]
+        print(f"position: {position}, orientation: {orientation}")
+        if place_id != -1:
+            pass
+        
+        self.publisher = self.create_publisher(PoseStamped, "/goal_pose", 10)
+        pose_stamped = PoseStamped()
+        pose_stamped.header.frame_id = "map"
+        pose_stamped.pose.position.x = position[0]
+        pose_stamped.pose.position.y = position[1]
+        pose_stamped.pose.position.z = position[2]
+        pose_stamped.pose.orientation.x = orientation[0]
+        pose_stamped.pose.orientation.y = orientation[1]
+        pose_stamped.pose.orientation.z = orientation[2]
+        pose_stamped.pose.orientation.w = orientation[3]
+        self.publisher.publish(pose_stamped)
+
         response.message = "Success"
         return response
+    
+    def init_argument(self):
+        """引数が必要なため、オーバーライド"""
+        return {"PLACE_ID": -1, "position": [8.65, 1.62, 0.0], "orientation": [0.0, 0.0, 0.280, 0.960]}
 
 
 class SubtaskGrasp(SubtaskNodeBase):
