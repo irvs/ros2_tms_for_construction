5a6bfb10cf20e77ca4999b0306b45c214f19b4cc
itsuka-to <itsuka@irvs.ait.kyushu-u.ac.jp>
2019-12-06 18:02:15 +0900

    added tms_ss_pozyx

---
+++ tms_ss/tms_ss_pozyx/setup.py
@@ -0,0 +1,42 @@
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'tms_ss_pozyx'
+setup(
+    name=package_name,
+    version='0.1.0',
+    # packages=find_packages(exclude=['test']),
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        # ('share/' + package_name, ['package.xml', 'test.launch.py']),
+        # (os.path.join('share', package_name, 'launch'), glob('*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Itsuka Tomoya',
+    author_email='itsuka@irvs.ait.kyushu-u.ac.jp',
+    maintainer='kawamura',
+    maintainer_email='Kawamura@irvs.ait.kyushu-u.ac,jp',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=('Package containing examples of how to use the rclpy API.'
+    ),
+    license='Apache License, Version 2.0',
+    test_suite='pytest',
+    entry_points={
+        'console_scripts': [
+            'tms_ss_pozyx = tms_ss_pozyx.tms_ss_pozyx:main',
+        ],
+    },
+)
+
+
