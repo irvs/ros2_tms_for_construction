a482d8f5923ceb4244819935c4e4429884692851
ryuichi-maeda <uryu44213@gmail.com>
2022-07-01 22:59:27 +0900

    Update: tms_db_util

--- tms_db/tms_db_manager/tms_db_manager/tms_db_util.py
+++ tms_db/tms_db_manager/tms_db_manager/tms_db_util.py
@@ -3,95 +3,131 @@ The Original Code is mongodb_store package's util.py
 http://www.ros.org/wiki/mongodb_store
 The Initial Developers of the Original Code are Chris Burbridge and Nick Hawes.
 This file is licensed under the MIT License.
-This file is modified by Minsoo Song.
+This file is modified by Minsoo Song and Ryuichi Maeda.
 """
 
+import array
+import numpy
 import rclpy
 import pymongo
-import json
-import copy
-from rclpy.node import Node
-from rclpy.callback_groups import CallbackGroup
-from std_msgs.msg import String
-from rclpy.executors import Executor
-from bson import json_util
-from bson.objectid import ObjectId
+from bson import Binary
+
+
+def connect_db(db_name: str, db_host: str = 'localhost', db_port: int = 27017) -> pymongo.database.Database:
+    """
+    Connect to MongoDB database.
+
+    Parameters
+    ----------
+    db_name : str
+        Target database name.
+
+    Returns
+    -------
+    pymongo.database.Database
+        Connected database.
+    """
+    client = pymongo.MongoClient(host=db_host, port=db_port)
+    db = client[db_name]
+    return db
 
 
 def msg_to_document(msg):
+    """
+    Convert supplied ROS msg to dictionary.
+
+    Parameters
+    ----------
+    msg : Any
+        ROS msg.
+
+    Returns
+    -------
+    dict
+        Dictionary converted from supplied ROS msg.
+    """
     msg_dict = {}
     slot_types = []
-    # print(msg)
     
     if hasattr(msg, 'slot_types'):
         slot_types = msg.slot_types
     else:
-        slot_types = [None] * len(msg.__slots__)          
-    # print(msg.__slots__)
-    for (attr, type) in zip(msg.__slots__, slot_types):
-        # print(msg.__slots__)
-        result = zip(msg.__slots__, slot_types)
+        slot_types = [None] * len(msg.__slots__)
 
-        resultSet = set(result)
-        # print(resultSet)
-        # print(attr)
-        msg_dict[attr[1:]] = sanitize_value(attr, getattr(msg, attr), type)
+    for (attr, type) in zip(msg.__slots__, slot_types):
+        msg_dict[attr[1:]] = sanitize_value(getattr(msg, attr), type)
         
     return msg_dict
     
 
-def sanitize_value(attr, v, type):
-    # if isinstance(v, str):
-    #     #if type == 'uint8[]':
-    #     #     v = Binary(v)
-    #     # else:
-    #     #     try:
-    #     #         v = unicode(v, "utf-8")
-    #     #     except UnicodeDecodeError as e:
-    #     #         v = Binary(v)
-    #     v = str(v)
+def sanitize_value(v, type):
+    """
+    Sanitize supplied value.
 
-    #     return v
+    Parameters
+    ----------
+    val : Any
+        Value to be sanitized.
 
-    # if isinstance(v, rclpy.Message):
-    #     return msg_to_document(v)
-    # if isinstance(v, genpy.rostime.Time):
-    #     return msg_to_document(v)
-    # elif isinstance(v, genpy.rostime.Duration):
-    #     return msg_to_document(v)
+    Returns
+    -------
+    Any
+        Sanitized value.
+    """
+
+    if isinstance(v, str) and type == 'uint8[]':
+        return Binary(v)
+
+    if isinstance(v, object) and hasattr(v, '__slots__'):
+        return msg_to_document(v)
+
+    if isinstance(v, numpy.ndarray) or isinstance(v, array.array):
+        v = v.tolist()
+
     if isinstance(v, list):
         result = []
         for t in v:
-            if hasattr(t, 'type'):
-                result.append(sanitize_value(None, t, t.type))
+            if hasattr(t, '_type'):
+                result.append(sanitize_value(t, t._type))
             else:
-                result.append(sanitize_value(None, t, None))
+                result.append(sanitize_value(t, None))
         return result
-    else:
         
-        return v
+    return v
 
 
-def check_connection(db_host, db_port):
+def check_connection(db_host: str, db_port: int) -> bool:
+    """
+    Check connection to mongod server.
+
+    Parameters
+    ----------
+    db_host : str
+        mongod host.
+    db_port : int
+        mongod port.
+
+    Returns
+    -------
+    bool
+        Result of whether the connection has been made or not.
+    """
     try:
-        from pymongo import Connection
-        Connection(db_host, db_port)
-        rclpy.loginfo("Connected to the ROS-TMS Database!")
-        #rclpy.shutdown()
+        pymongo.MongoClient(db_host, db_port)
+        rclpy.loginfo("Connected to the ROS2-TMS Database!")
         return True
-    except Exception as e:
-        #self.get_logger().info('gError: "%s"' % str(e))
-        #self.get_logger().info('Could not connect to mongo server "%s":"%d"' % (db_host, db_port))
-        #rclpy.shutdown()
+    except:
         return False
 
 
+# TODO fix for using json.
 def document_to_msg(document, TYPE):
     msg = TYPE()
     _fill_msg(msg, document)
     return msg
 
 
+# TODO fix for using json.
 def _fill_msg(msg, dic):
     for i in dic:
         if isinstance(dic[i], dict):
@@ -153,3 +189,43 @@ def _fill_msg(msg, dic):
 
             if i != "require_tag" and i != "noun" and i != "error_announce" and i != "tokens":
                 setattr(msg, i, dic[i])
+
+
+def get_collection_by_id(db: pymongo.database.Database, id: int) -> pymongo.collection.Collection:
+    """
+    Get collection based on ID.
+
+    Parameters
+    ----------
+    db : pymongo.database.Database
+        Targent MongoDB database.
+    id : int
+        Target ID.
+
+    Returns
+    -------
+    pymongo.collection.Collection
+        MongoDB collection.
+    """
+    if id >= 11000:
+        return db['machine']
+    elif id >= 10000:
+        return db['state']
+    elif id >= 9000:
+        return db['subtask']
+    elif id >= 8000:
+        return db['task']
+    elif id >= 7000:
+        return db['object']
+    elif id >= 6000:
+        return db['furniture']
+    elif id >= 5000:
+        return db['space']
+    elif id >= 4000:
+        return db['structure']
+    elif id >= 3000:
+        return db['sensor']
+    elif id >= 2000:
+        return db['robot']
+    else:
+        return db['person']
