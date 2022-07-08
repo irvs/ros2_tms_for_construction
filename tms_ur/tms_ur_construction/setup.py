2aaca62cfcebd80a1443adfa46d1a2fd9a630674
ryuichi-maeda <uryu44213@gmail.com>
2022-07-08 12:28:17 +0900

    Update: setup.py for new created nodes

--- tms_ur/tms_ur_construction/setup.py
+++ tms_ur/tms_ur_construction/setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+import os
 from setuptools import setup
 
 package_name = 'tms_ur_construction'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*_launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +23,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'tms_ur_construction_ground = tms_ur_construction.tms_ur_construction_ground:main',
+            'tms_ur_cv_odom = tms_ur_construction.tms_ur_cv_odom:main',
+            'tms_ur_construction_terrain = tms_ur_construction.tms_ur_construction_terrain:main',
         ],
     },
 )
