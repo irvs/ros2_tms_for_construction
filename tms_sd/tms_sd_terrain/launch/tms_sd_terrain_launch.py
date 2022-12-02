2fd970cd58ddb3376dd5900a796743d1d1e0409e
ryuichi-maeda <uryu44213@gmail.com>
2022-12-02 17:52:11 +0900

    Change: from sd_terrain to sd_static_terrain

--- tms_sd/tms_sd_terrain/launch/tms_sd_terrain_launch.py
+++ tms_sd/tms_sd_terrain/launch/tms_sd_terrain_launch.py
@@ -4,8 +4,8 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    input_pointcloud2 = DeclareLaunchArgument(
-        'input/pointcloud2', default_value='input/pointcloud2'
+    input_terrain_static_pointcloud2 = DeclareLaunchArgument(
+        'input/terrain/static/pointcloud2', default_value='input/terrain/static/pointcloud2'
     )
     filename = DeclareLaunchArgument(
         'filename', default_value='filename'
@@ -13,10 +13,10 @@ def generate_launch_description():
 
     tms_sd_terrain_node = Node(
         package='tms_sd_terrain',
-        executable='tms_sd_terrain',
+        executable='tms_sd_static_terrain',
         output='screen',
         remappings=[
-            ('~/input/pointcloud2', LaunchConfiguration('input/pointcloud2')),
+            ('~/input/terrain/static/pointcloud2', LaunchConfiguration('input/terrain/static/pointcloud2')),
         ],
         parameters=[{
             'filename': LaunchConfiguration('filename'),
@@ -24,7 +24,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        input_pointcloud2,
+        input_terrain_static_pointcloud2,
         filename,
         tms_sd_terrain_node,
     ])
