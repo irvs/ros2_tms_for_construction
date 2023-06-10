d7a25afecb2ed418c2e3ead5b33a9347ac9c8de6
ryuichi-maeda <uryu44213@gmail.com>
2023-06-10 16:10:37 +0900

    Add: launch for demo

---
+++ tms_sp/tms_sp_machine_odom/launch/tms_sp_machine_odom_demo_launch.py
@@ -0,0 +1,107 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Arguments
+    to_frame = DeclareLaunchArgument("to_frame", default_value="world")
+
+    # Nodes
+    tms_sp_machine_odom_node1 = Node(
+        name="tms_sp_machine_odom1",
+        package="tms_sp_machine_odom",
+        executable="tms_sp_machine_odom",
+        output="screen",
+        remappings=[
+            ("~/input/odom", "/cd_0/odom"),
+        ],
+        parameters=[
+            {
+                "machine_name": "machine1",
+            },
+            {
+                "to_frame": LaunchConfiguration("to_frame"),
+            },
+        ],
+    )
+    tms_sp_machine_odom_node2 = Node(
+        name="tms_sp_machine_odom2",
+        package="tms_sp_machine_odom",
+        executable="tms_sp_machine_odom",
+        output="screen",
+        remappings=[
+            ("~/input/odom", "/cd_1/odom"),
+        ],
+        parameters=[
+            {
+                "machine_name": "machine2",
+            },
+            {
+                "to_frame": LaunchConfiguration("to_frame"),
+            },
+        ],
+    )
+    tms_sp_machine_odom_node3 = Node(
+        name="tms_sp_machine_odom3",
+        package="tms_sp_machine_odom",
+        executable="tms_sp_machine_odom",
+        output="screen",
+        remappings=[
+            ("~/input/odom", "/cd_2/odom"),
+        ],
+        parameters=[
+            {
+                "machine_name": "machine3",
+            },
+            {
+                "to_frame": LaunchConfiguration("to_frame"),
+            },
+        ],
+    )
+    tms_sp_machine_odom_node4 = Node(
+        name="tms_sp_machine_odom4",
+        package="tms_sp_machine_odom",
+        executable="tms_sp_machine_odom",
+        output="screen",
+        remappings=[
+            ("~/input/odom", "/cd_3/odom"),
+        ],
+        parameters=[
+            {
+                "machine_name": "machine4",
+            },
+            {
+                "to_frame": LaunchConfiguration("to_frame"),
+            },
+        ],
+    )
+    tms_sp_machine_odom_node5 = Node(
+        name="tms_sp_machine_odom5",
+        package="tms_sp_machine_odom",
+        executable="tms_sp_machine_odom",
+        output="screen",
+        remappings=[
+            ("~/input/odom", "/cd_4/odom"),
+        ],
+        parameters=[
+            {
+                "machine_name": "machine5",
+            },
+            {
+                "to_frame": LaunchConfiguration("to_frame"),
+            },
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            to_frame,
+            tms_sp_machine_odom_node1,
+            tms_sp_machine_odom_node2,
+            tms_sp_machine_odom_node3,
+            tms_sp_machine_odom_node4,
+            tms_sp_machine_odom_node5,
+        ]
+    )
