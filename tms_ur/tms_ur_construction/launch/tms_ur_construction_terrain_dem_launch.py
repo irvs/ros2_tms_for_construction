eb1b5cad86d1d51be27f1215cbfbe4783539cbb9
ryuichi-maeda <uryu44213@gmail.com>
2023-08-26 16:19:54 +0900

    Add: license description

--- tms_ur/tms_ur_construction/launch/tms_ur_construction_terrain_dem_launch.py
+++ tms_ur/tms_ur_construction/launch/tms_ur_construction_terrain_dem_launch.py
@@ -1,37 +1,54 @@
+# Copyright 2023, IRVS Laboratory, Kyushu University, Japan.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     # Arguments
     output_dem = DeclareLaunchArgument(
-        'output/terrain/dem_srv', default_value='output/terrain/dem_srv'
+        "output/terrain/dem_srv", default_value="output/terrain/dem_srv"
     )
     filename_dem = DeclareLaunchArgument(
-        'filename_dem', default_value='filename_dem.npy'
+        "filename_dem", default_value="filename_dem.npy"
     )
-    resolution = DeclareLaunchArgument(
-        'resolution', default_value='0.1'
-    )
+    resolution = DeclareLaunchArgument("resolution", default_value="0.1")
 
     # Nodes
     tms_ur_construction_terrain_dem_node = Node(
-        package='tms_ur_construction',
-        executable='tms_ur_construction_terrain_dem',
-        output='screen',
+        package="tms_ur_construction",
+        executable="tms_ur_construction_terrain_dem",
+        output="screen",
         remappings=[
-            ('~/output/terrain/dem_srv', LaunchConfiguration('output/terrain/dem_srv')),
+            ("~/output/terrain/dem_srv", LaunchConfiguration("output/terrain/dem_srv")),
         ],
-        parameters=[{
-            'filename_dem': LaunchConfiguration('filename_dem'),
-            'resolution': LaunchConfiguration('resolution')
-        }]
+        parameters=[
+            {
+                "filename_dem": LaunchConfiguration("filename_dem"),
+                "resolution": LaunchConfiguration("resolution"),
+            }
+        ],
     )
 
-    return LaunchDescription([
-        output_dem,
-        filename_dem,
-        resolution,
-        tms_ur_construction_terrain_dem_node,
-    ])
+    return LaunchDescription(
+        [
+            output_dem,
+            filename_dem,
+            resolution,
+            tms_ur_construction_terrain_dem_node,
+        ]
+    )
--- tms_ur/tms_ur_test/tms_ur_test/terrain_dem_cli_pub.py
+++ tms_ur/tms_ur_test/tms_ur_test/terrain_dem_cli_pub.py
@@ -1,3 +1,17 @@
+# Copyright 2023, IRVS Laboratory, Kyushu University, Japan.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid
@@ -6,12 +20,11 @@ from tms_msg_db.srv import DemSrv
 
 
 class MinimalClientAsync(Node):
-
     def __init__(self):
-        super().__init__('terrain_dem_cli')
-        self.cli = self.create_client(DemSrv, '/output/terrain/dem_srv')
+        super().__init__("terrain_dem_cli")
+        self.cli = self.create_client(DemSrv, "/output/terrain/dem_srv")
         while not self.cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
+            self.get_logger().info("service not available, waiting again...")
         self.req = DemSrv.Request()
 
     def send_request(self):
@@ -21,10 +34,13 @@ class MinimalClientAsync(Node):
 
 
 class MinimalPublisher(Node):
-
     def __init__(self, occupancy_grid: OccupancyGrid):
-        super().__init__('terrain_dem_pub')
-        self.publisher_ = self.create_publisher(OccupancyGrid, '/tms_ur_construction_terrain_dem/output/terrain/dem/demo', 10)
+        super().__init__("terrain_dem_pub")
+        self.publisher_ = self.create_publisher(
+            OccupancyGrid,
+            "/tms_ur_construction_terrain_dem/output/terrain/dem/demo",
+            10,
+        )
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.occupancy_grid = occupancy_grid
@@ -51,5 +67,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
