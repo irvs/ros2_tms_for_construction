0356687989c9fac7c302e291d0ddb4f0bc1d6456
Ryuichi Maeda <63947554+ryuichi-maeda@users.noreply.github.com>
2023-08-27 18:44:00 +0900

    Merge tms_sp_machine_odom and tms_sp_machine_points to tms_sp_machine (#52)
    * Merge: tms_sp_machine_odom and tms_sp_machine_points to tms_sp_machine
    
    * Fix: documents

--- tms_sp/tms_sp_machine/launch/tms_sp_machine_odom_launch.py
+++ tms_sp/tms_sp_machine/launch/tms_sp_machine_odom_launch.py
@@ -1,11 +1,11 @@
 # Copyright 2023, IRVS Laboratory, Kyushu University, Japan.
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,23 +19,32 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Arguments
-    input_points = DeclareLaunchArgument(
-        "input/machine_points", default_value="/input/machine_points"
-    )
+    input_odom = DeclareLaunchArgument("input/odom", default_value="input/odom")
+    machine_name = DeclareLaunchArgument("machine_name", default_value="machine_name")
     to_frame = DeclareLaunchArgument("to_frame", default_value="world")
 
-    # Nodes
-    machine_points = Node(
-        package="tms_sp_machine_points",
-        executable="tms_sp_machine_points",
+    tms_sp_machine_odom_node = Node(
+        package="tms_sp_machine",
+        executable="tms_sp_machine_odom.py",
         output="screen",
         remappings=[
-            ("~/input/machine_points", LaunchConfiguration("input/machine_points")),
+            ("~/input/odom", LaunchConfiguration("input/odom")),
         ],
         parameters=[
-            {"to_frame": LaunchConfiguration("to_frame")},
+            {
+                "machine_name": LaunchConfiguration("machine_name"),
+            },
+            {
+                "to_frame": LaunchConfiguration("to_frame"),
+            },
         ],
     )
 
-    return LaunchDescription([input_points, to_frame, machine_points])
+    return LaunchDescription(
+        [
+            input_odom,
+            machine_name,
+            to_frame,
+            tms_sp_machine_odom_node,
+        ]
+    )
