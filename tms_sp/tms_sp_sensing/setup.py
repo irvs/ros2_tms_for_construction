d42af7ffe7ee1a4bbedb195c253efd80773d64da
kasahara <kasahara.yuichiro.res@gmail.com>
2023-10-15 19:09:57 +0900

    Add program to update dynamic param from anotherpc

---
+++ tms_sp/tms_sp_sensing/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'tms_sp_sensing'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='common',
+    maintainer_email='kasahara.yuichiro.res@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "tms_sp_sensing_sample = tms_sp_sensing.tms_sp_sensing_sample:main",
+        ],
+    },
+)
