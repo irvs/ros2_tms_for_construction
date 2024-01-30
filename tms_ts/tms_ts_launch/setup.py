ab4ebba957162ee7dcccaefac1128fe7e424875d
ChiroruNeko <koshi2660@gmail.com>
2024-01-30 20:14:02 +0900

    Fix to dismiss the warning

--- tms_ts/tms_ts_launch/setup.py
+++ tms_ts/tms_ts_launch/setup.py
@@ -13,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name), ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
