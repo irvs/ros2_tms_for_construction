7d94f1c46ec206d102e1c15dbb093eeef771c2e2
ChiroruNeko <koshi2660@gmail.com>
2024-03-03 22:11:16 +0900

    Fix for demo

--- tms_ts/tms_ts_launch/launch/tms_ts_construction.launch.py
+++ tms_ts/tms_ts_launch/launch/tms_ts_construction.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
                   package='tms_ur_button_input', 
                   executable='tms_ur_button',
                   output='screen', 
-                  parameters=[{"task_id": 15}]), # You must define task_id that you want to execute. Default task_id is 2.
+                  parameters=[{"task_id": 16}]), # You must define task_id that you want to execute. Default task_id is 2.
             
             # subtasks
             Node(
