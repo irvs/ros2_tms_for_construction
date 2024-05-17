3f21abfbabbcb4f781bfcb8d2cd7f3974e0c40e2
AKota3 <167172332+AKota3@users.noreply.github.com>
2024-05-17 17:00:52 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ sudo apt update && sudo apt install -y curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
-sudo apt upgrade
+sudo apt -y upgrade
 sudo apt install -y ros-humble-desktop
 sudo apt install -y ros-humble-ros-base
 sudo apt install -y ros-dev-tools
