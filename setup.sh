31ec362f99fc4802bdcc729d0ded685e78960706
AKota3 <167172332+AKota3@users.noreply.github.com>
2024-05-17 17:04:51 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,15 @@
 # This file is used to setup the environment for ROS2-TMS for Construction.
 
-##setup.shの作成手順################################################################################
-
-# この下からros2-tms for constructionのreadme資料を参考に環境構築用のスクリプトを作成して下さい
-# ros2の環境構築の一部のみ参考に示しておきます
-# このファイルの実行コマンドは以下のとおり
-
-# cd ~/[woekspace]/src/ros2_tms_for_construction
-# sudo chmod +x setup.sh
-# sudo ./setup.sh
-
-# 上記コマンドを実行すると、以下のコマンドをターミナル上で打ち込んだ際と同様な出力が得られます
-
-# locale  # check for UTF-8
-
-# sudo apt update && sudo apt install locales
-# sudo locale-gen en_US en_US.UTF-8
-# sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-# export LANG=en_US.UTF-8
-
-# locale  # verify settings
+# Copyright 2023, IRVS Laboratory, Kyushu University, Japan.
+# //  Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ##################################################################################################
 
 # Setup ROS2
