1c56da7a430632e52128a23c58919650ed702085
AKota3 <167172332+AKota3@users.noreply.github.com>
2024-05-17 15:21:28 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ locale  # verify settings
 
 sudo apt install software-properties-common
 sudo add-apt-repository universe
-sudo apt update && sudo apt install curl -y
+sudo apt update && sudo apt install -y curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
