c41ae56649591fe86ef6f9467283bc93095e6d6d
AKota3 <167172332+AKota3@users.noreply.github.com>
2024-05-20 10:24:29 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | \
    --dearmor
    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install -y mongodb-org
 echo "mongodb-org hold" | sudo dpkg --set-selections
