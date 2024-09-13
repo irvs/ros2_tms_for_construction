3cb12eddf798790c9b7ca5823dc9d4093a4bd72b
kasahara-san <kasahara.yuichiro.res@gmail.com>
2024-09-13 16:31:50 +0900

    Update: 一時保存

--- tms_ts/tms_ts_subtask/src/ic120/subtask_ic120_release_soil_server.cpp
+++ tms_ts/tms_ts_subtask/src/ic120/subtask_ic120_release_soil_server.cpp
@@ -21,7 +21,7 @@ using namespace std::chrono_literals;
 SubtaskIc120ReleaseSoil::SubtaskIc120ReleaseSoil() : SubtaskNodeBase("st_ic120_release_soil_node")
 {
   // publisher_ = this->create_publisher<std_msgs::msg::Float64>("/ic120/vessel/cmd", 10);
-  client_ = this->create_client<tms_msg_ts::srv::TargetAngle>("/ic120/set_target_angle");
+  client_ = this->create_client<com3_msgs::srv::DumpUp>("/ic120/vessel_target_pos");
   this->action_server_ = rclcpp_action::create_server<tms_msg_ts::action::LeafNodeBase>(
       this, "st_ic120_release_soil",
       std::bind(&SubtaskIc120ReleaseSoil::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
@@ -53,13 +53,13 @@ void SubtaskIc120ReleaseSoil::execute(const std::shared_ptr<GoalHandle> goal_han
   RCLCPP_INFO(this->get_logger(), "subtask is executing...");
   rclcpp::Rate loop_rate(1);
   auto feedback = std::make_shared<tms_msg_ts::action::LeafNodeBase::Feedback>();
-  auto& status = feedback->status;
+  // auto& status = feedback->status;
   auto result = std::make_shared<tms_msg_ts::action::LeafNodeBase::Result>();
 
-  auto request = std::make_shared<tms_msg_ts::srv::TargetAngle::Request>();
+  auto request = std::make_shared<com3_msgs::srv::DumpUp::Request>();
   request->vessel_angle = parameters["target_angle"];
 
-  while (!client_->wait_for_service(3s))
+  while (!client_->wait_for_service(20s))
   {
     if (!rclcpp::ok())
     {
@@ -73,14 +73,14 @@ void SubtaskIc120ReleaseSoil::execute(const std::shared_ptr<GoalHandle> goal_han
 
   auto future = client_->async_send_request(request);
 
-    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), future) == rclcpp::FutureReturnCode::SUCCESS)
+  if (future.wait_for(60s) == std::future_status::ready)
   {
     auto response = future.get();
     if (response->result)
     {
       result->result = true;
       goal_handle->succeed(result);
-      RCLCPP_INFO(this->get_logger(), "subtask execution is succeeded");
+      RCLCPP_INFO(this->get_logger(), "subtask execution succeeded");
     }
     else
     {
@@ -95,45 +95,6 @@ void SubtaskIc120ReleaseSoil::execute(const std::shared_ptr<GoalHandle> goal_han
     goal_handle->abort(result);
     RCLCPP_ERROR(this->get_logger(), "Failed to call service.");
   }
-
-  // double deg = 0;
-  // std_msgs::msg::Float64 msg_rad;
-
-  // while (deg >= goal_pos)
-  // {
-  //   if (goal_handle->is_canceling())
-  //   {
-  //     result->result = false;
-  //     goal_handle->canceled(result);
-  //     RCLCPP_INFO(this->get_logger(), "subtask execution is canceled");
-  //     return;
-  //   }
-  //   deg += double(goal_pos / float(5.0));
-  //   msg_rad.data = float(deg * float(M_PI / 180));
-  //   status = deg;
-  //   goal_handle->publish_feedback(feedback);
-  //   publisher_->publish(msg_rad);
-  //   RCLCPP_INFO_STREAM(this->get_logger(), "Publishing vessel position: " << deg << " [deg]");
-  //   sleep(1);
-  // }
-
-  // while (deg <= 0.0)
-  // {
-  //   if (goal_handle->is_canceling())
-  //   {
-  //     result->result = false;
-  //     goal_handle->canceled(result);
-  //     RCLCPP_INFO(this->get_logger(), "subtask execution is canceled");
-  //     return;
-  //   }
-  //   deg -= double(goal_pos / float(5.0));
-  //   msg_rad.data = float(deg * float(M_PI / 180));
-  //   status = deg;
-  //   goal_handle->publish_feedback(feedback);
-  //   publisher_->publish(msg_rad);
-  //   RCLCPP_INFO_STREAM(this->get_logger(), "Publishing vessel position: " << deg << " [deg]");
-  //   sleep(1);
-  // }
 
 }
 
