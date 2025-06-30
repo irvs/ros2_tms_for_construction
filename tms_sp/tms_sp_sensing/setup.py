12d2ae6ebfaaba034cf3bab1c0063dc32729e371
kasahara-san <kasahara.yuichiro.res@gmail.com>
2025-06-30 14:04:58 +0900

    Update: データ型及び対応するTMS_SPのノードを追加

--- tms_sp/tms_sp_sensing/setup.py
+++ tms_sp/tms_sp_sensing/setup.py
@@ -24,7 +24,18 @@ setup(
             "tms_sp_zx200_collision_objects = tms_sp_sensing.tms_sp_zx200_collision_objects_ic120:main",
             "sample = tms_sp_sensing.sample:main",
             "tms_sp_flgs = tms_sp_sensing.tms_sp_flgs:main",
-            "tms_sp_flgs_202506 = tms_sp_sensing.tms_sp_flgs_202506:main"
+            "tms_sp_flgs_202506 = tms_sp_sensing.tms_sp_flgs_202506:main",
+
+            "tms_sp_navigate_anywhere = tms_sp_sensing.tms_sp_navigate_anywhere:main",
+            "tms_sp_navigate_anywhere_deg = tms_sp_sensing.tms_sp_navigate_anywhere_deg:main",
+            "tms_sp_follow_waypoints = tms_sp_sensing.tms_sp_follow_waypoints:main",
+            "tms_sp_follow_waypoints_deg = tms_sp_sensing.tms_sp_follow_waypoints_deg:main",
+            "tms_sp_navigate_through_poses = tms_sp_sensing.tms_sp_navigate_through_poses:main",
+            "tms_sp_navigate_through_poses_deg = tms_sp_sensing.tms_sp_navigate_through_poses_deg:main",
+            "tms_sp_change_pose = tms_sp_sensing.tms_sp_change_pose:main",
+            "tms_sp_excavate_simple = tms_sp_sensing.tms_sp_excavate_simple:main",
+            "tms_sp_release_simple = tms_sp_sensing.tms_sp_release_simple:main",
+
         ],
     },
 )
