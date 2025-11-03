6fb161f81f52967a6c74b3dc02fed7b9a9cdfba2
kasahara-san <kasahara.yuichiro.res@gmail.com>
2025-11-03 12:55:12 +0900

    Add: ナビゲーション用SMノードを拡充。動作未確認

--- tms_ts/tms_ts_sm/tms_behaviors/tms_flexbe_states/setup.py
+++ tms_ts/tms_ts_sm/tms_behaviors/tms_flexbe_states/setup.py
@@ -27,8 +27,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'example_action_state = tms_flexbe_states.example_action_state',
-            'example_state = tms_flexbe_states.example_state',
         ],
     },
 )
